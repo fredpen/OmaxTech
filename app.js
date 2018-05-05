47dfdba6eb4cec4c1b072d92192e2e47ec188008
fred <fredricksola@yahoo.com>
2018-05-05 20:17:03 +0100

    set uo all the folder structure fully customised the contact page

--- app.js
+++ app.js
@@ -1,32 +1,46 @@
 var express = require("express"),
    bodyParser = require("body-parser"),
+   nodemailer = require('nodemailer'),
    app = express();
 
+// using the app dependencies
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
    extended: true
 }));
 app.use(express.static(__dirname + "/public"));
 
-// the landing page
+
+// the routes
+// the landing page route
 app.get("/", function(req, res) {
    res.render("landing")
 });
 
-// the home page
+// the home page route
 app.get("/home", function(req, res) {
    res.render("home")
 });
 
-// about page
+// about page route
 app.get("/about", function(req, res) {
    res.render("about")
 });
 
-// contact page
+// contact page route
 app.get("/contact", function(req, res) {
    res.render("contact")
 });
+
+// electrical installation route
+app.get("/services/electrical_installation", function(req, res) {
+   res.render("services/electric")
+});
+
+// fire prevention and protection route
+app.get("/services/fire", function(req, res) {
+   res.render("services/fire")
+})
 
 // contact logic
 app.post("/contact", function(req, res) {
@@ -34,7 +48,31 @@ app.post("/contact", function(req, res) {
       email = req.body.email,
       content = req.body.content,
       number = req.body.number;
-   res.redirect("home")
+
+   var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+         user: 'fredrickksola@gmail.com',
+         pass: 'lautech@60'
+      }
+   });
+
+   var mailOptions = {
+      from: email,
+      to: 'fredricksola@yahoo.com',
+      subject: "An email from your website Omax technologies",
+      text: name,
+      html: content, name
+   };
+
+   transporter.sendMail(mailOptions, function(error, info) {
+      if (error) {
+         console.log(error);
+      } else {
+         console.log('Email sent: ' + info.response);
+      }
+   });
+   res.redirect("/")
 });
 
 
